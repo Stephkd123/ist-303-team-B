cf90eb56afaffa068b7aa15f314bdda64592827b
Rohini Vishwanathan <rohini.vishwanathan@cgu.edu>
2025-04-23 18:17:41 -0700

    Update app.py
    Integrated secure login and signup functionality into app.py

--- la_olympics_route_app/app.py
+++ la_olympics_route_app/app.py
@@ -1,3 +1,6 @@
+from flask import flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect, session, url_for
 import pandas as pd
 import os
@@ -5,6 +8,13 @@ import googlemaps
 from polyline import decode as decode_polyline
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 app.secret_key = "supersecret"
 
 gmaps = googlemaps.Client(key="AIzaSyBqrXhBRKtjniEN453TybIXB0lnuawgk20")
@@ -153,6 +163,37 @@ def delete_route(index):
         saved.pop(index)
         session["saved_routes"] = saved
     return redirect("/my_routes")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash("Login successful!", "success")
+            return redirect('/')
+        else:
+            flash("Invalid credentials", "danger")
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        if User.query.filter_by(username=username).first():
+            flash("Username already exists", "warning")
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Signup successful! Please log in.", "success")
+            return redirect('/login')
+    return render_template('signup.html')
+
 
 # add logout function
 @app.route("/logout")
